3cb8c97534f0f2220e5b55fe18fefa960dbd4094
Sunshinepunch <78851729+Sunshinepunch@users.noreply.github.com>
2021-08-13 11:59:52 -0700

    display questions and answers appropriately

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-function add (answer1,answer2,answer3) {
-  return (answer1 + answer2 + answer3);
+function add (answer1,answer2,answer3,answer4,answer5) {
+  return (answer1 + answer2 + answer3 + answer4 + answer5);
 }
 
 
@@ -15,13 +15,15 @@ $(document).ready(function() {
     let answer1 = parseInt($("input:radio[name=ques1]:checked").val());
     let answer2 = parseInt($("input:radio[name=ques2]:checked").val());
     let answer3 = parseInt($("input:radio[name=ques3]:checked").val());
-    let result = add(answer1, answer2, answer3);
+    let answer4 = parseInt($("input:radio[name=ques4]:checked").val());
+    let answer5 = parseInt($("input:radio[name=ques5]:checked").val());
+    let result = add(answer1, answer2, answer3, answer4, answer5);
 
-    if (result <= 3) {
+    if (result <= 5) {
       $("#python").show();
-    } else if (result > 3 && result <= 6) {
+    } else if (result > 5 && result <= 10) {
       $("#javascript").show();
-    } else if (result > 6) {
+    } else if (result > 10) {
       $("#csharp").show();
     }
 
