de1e304a01e5283c14f1bb0952b15ece65017ac3
Sunshinepunch <78851729+Sunshinepunch@users.noreply.github.com>
2021-08-13 11:08:55 -0700

    will display skeleton of working quiz and text results. (WIP, github showing errors when trying to push to commit)

--- js/scripts.js
+++ js/scripts.js
@@ -12,17 +12,17 @@ $(document).ready(function() {
 
   $("form#quiz").submit(function(event) {
     event.preventDefault();
-    let answer1 = parseInt($("input:radio[name=chargingCreature]:checked").val());
-    let answer2 = parseInt($("input:radio[name=essay]:checked").val());
-    let answer3 = parseInt($("input:radio[name=money]:checked").val());
+    let answer1 = parseInt($("input:radio[name=ques1]:checked").val());
+    let answer2 = parseInt($("input:radio[name=ques2]:checked").val());
+    let answer3 = parseInt($("input:radio[name=ques3]:checked").val());
     let result = add(answer1, answer2, answer3);
 
     if (result <= 3) {
-      $("#harry").show();
-    } else if (result > 3 && result <= 7) {
-      $("#ron").show();
-    } else if (result > 7) {
-      $("#hermione").show();
+      $("#python").show();
+    } else if (result > 3 && result <= 6) {
+      $("#javascript").show();
+    } else if (result > 6) {
+      $("#csharp").show();
     }
 
     $(".quiz").hide();
